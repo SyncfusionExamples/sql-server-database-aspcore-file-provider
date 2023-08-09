e33c1f2f2f8e6f47a300b713b9f001c3ae6c1362
sivakumar3442 <sivakumar.sf3442@gmail.com>
2023-08-09 13:15:42 +0530

    Directory-traversal issue

--- Models/SQLFileProvider.cs
+++ Models/SQLFileProvider.cs
@@ -372,11 +372,14 @@ namespace Syncfusion.EJ2.FileManager.Base.SQLFileProvider
             {
                 FileManagerDirectoryContent createData = new FileManagerDirectoryContent();
 
-                AccessPermission createPermission = GetPermission(data[0].Id, data[0].ParentID, data[0].Name, data[0].IsFile, path);
-                if (createPermission != null && (!createPermission.Read || !createPermission.WriteContents))
+                if(GetPermission(data[0].Id, data[0].ParentID, data[0].Name, data[0].IsFile, path)!=null)
                 {
-                    accessMessage = createPermission.Message;                       
-                    throw new UnauthorizedAccessException("'" + data[0].Name + "' is not accessible. You need permission to perform the writeContents action.");   
+                    AccessPermission createPermission = GetPermission(data[0].Id, data[0].ParentID, data[0].Name, data[0].IsFile, path);
+                    if (createPermission != null && (!createPermission.Read || !createPermission.WriteContents))
+                    {
+                        accessMessage = createPermission.Message;                       
+                        throw new UnauthorizedAccessException("'" + data[0].Name + "' is not accessible. You need permission to perform the writeContents action.");   
+                    }
                 }
 
                 sqlConnection = setSQLDBConnection();
