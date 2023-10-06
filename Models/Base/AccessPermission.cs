031b680c22e3184bebca364269032d243dbca7f4
sivakumar3442 <sivakumar.sf3442@gmail.com>
2023-10-06 18:48:32 +0530

    Updated the AccessPermission model

--- Models/Base/AccessPermission.cs
+++ Models/Base/AccessPermission.cs
@@ -7,37 +7,37 @@ namespace Syncfusion.EJ2.FileManager.Base
     public class AccessPermission
     {
         /// <summary>
-        /// Specifies access to copy a file or folder.
+        /// Gets or sets access to copy a file or folder.
         /// </summary>
         public bool Copy { get; set; } = true;
 
         /// <summary>
-        /// Specifies permission to download a file or folder.
+        /// Gets or sets permission to download a file or folder.
         /// </summary>
         public bool Download { get; set; } = true;
 
         /// <summary>
-        /// Specifies permission to write a file or folder.
+        /// Gets or sets permission to write a file or folder.
         /// </summary>
         public bool Write { get; set; } = true;
 
         /// <summary>
-        /// Specifies permission to write the content of folder.
+        /// Gets or sets permission to write the content of folder.
         /// </summary>
         public bool WriteContents { get; set; } = true;
 
         /// <summary>
-        /// Specifies access to read a file or folder.
+        /// Gets or sets access to read a file or folder.
         /// </summary>
         public bool Read { get; set; } = true;
         
         /// <summary>
-        /// Specifies permission to upload to the folder.
+        /// Gets or sets permission to upload to the folder.
         /// </summary>
         public bool Upload { get; set; } = true;
 
         /// <summary>
-        /// Specifies the access message.
+        /// Gets or sets the access message.
         /// </summary>
         public string Message { get; set; } = string.Empty;
     }
