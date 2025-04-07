16e5661f77cf2f1399aa5249d6bc566c7b79ca09
sivakumar <93644655+sivakumars3442@users.noreply.github.com>
2025-04-07 16:39:03 +0530

    918603: Updated the service with security changes.

---
+++ Models/Base/ErrorDetails.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Syncfusion.EJ2.FileManager.Base
+{
+    public class ErrorDetails
+    {
+
+        public string Code { get; set; }
+
+        public string Message { get; set; }
+
+        public IEnumerable<string> FileExists { get; set; }
+    }
+}
---
+++ Models/Base/ImageSize.cs
@@ -0,0 +1,8 @@
+﻿namespace Syncfusion.EJ2.FileManager.Base
+{
+    public class ImageSize
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+    }
+}
